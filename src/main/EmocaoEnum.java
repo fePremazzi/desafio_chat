c9739444eb3e35aee3750de1658c54efc89047bc
fellipe Premazzi <felliperego@hotmail.com>
2019-08-07 20:54:30 -0300

    initial commit

---
+++ src/main/EmocaoEnum.java
@@ -0,0 +1,6 @@
+package main;
+
+public enum EmocaoEnum {
+	BEM,
+	MAL
+}
---
+++ src/main/Pessoa.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.util.Date;
+
+public abstract class Pessoa {
+	
+	private String nome;
+	private int idade;
+	private Date dataNascimento;
+	
+	public abstract EmocaoEnum getEmocao();
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+}
